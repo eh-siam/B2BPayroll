aeeec757063272ec98ddb9fe8a593073fe17c0e0
EMDADUL-HAQUE-SIAM <ehsiam75@gmail.com>
2025-07-02 19:13:42 +0600

    feat(home): add welcome text to home screen

--- app/src/main/java/com/simec/b2bpayroll/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/simec/b2bpayroll/presentation/screen/home/HomeScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
